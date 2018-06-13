f071e25eed759a0f1d37ebe0f9a645aa940031a0
Mads Holten Rasmussen <mads.h.rasmussen@gmail.com>
2018-06-13 18:31:29 +0200

    Moved docs to here

---
+++ config.js
@@ -0,0 +1,64 @@
+var respecConfig = {
+    specStatus: "CG-DRAFT",
+    shortName: "vocab-bot",
+    //publishDate:  "2015-05-18",
+    //previousPublishDate: "2014-03-27",
+    //previousMaturity: "FPWD",
+    //previousURI: "http://www.w3.org/TR/2014/WD-tabular-data-model-20140327/",
+    edDraftURI: "https://w3c-lbd-cg.github.io/lbd/bot/",
+    // lcEnd: "3000-01-01",
+    // crEnd: "3000-01-01",
+    editors: [
+    {
+        name: "Mads Holten Rasmussen",
+        company: "Niras | Technical University of Denmark",
+        url: "http://www.dtu.dk/english/service/phonebook/person?id=42843&cpid=202565&tab=2&qt=dtupublicationquery",
+        companyURL: "http://www.niras.com/"
+    },
+    {
+        name: "Pieter Pauwels",
+        company: "Ghent University",
+        url: "https://www.ugent.be/ea/architectuur/en/contact/staff-members/pieter-pauwels",
+        companyURL: "https://www.ugent.be/en"
+    }],
+	otherLinks: [
+      {
+      key: "Contributors (ordered alphabetically)",
+      data: [
+      {
+        value: "Maxime Lefrançois, École Nationale Supérieure des Mines de Saint-Étienne"
+      },{
+        value: "Georg Ferdinand Schneider, Fraunhofer Institute for Building Physics, Technische Hochschule Nürnberg"
+      }]
+      },
+      ],
+    wg: "Linked Building Data Community Group",
+    wgURI: "https://www.w3.org/community/lbd/",
+    wgPublicList: "",
+    implementationReportURI: "https://w3c-lbd-cg.github.io/lbd/bot/",
+    inlineCSS: true,
+    noIDLIn: true,
+    noLegacyStyle: false,
+      logos: [
+      {
+        src: "https://www.w3.org/StyleSheets/TR/2016/logos/W3C",
+        alt: "W3C",
+        height: "48",
+        width: "72",
+        url: "https://www.w3.org/"
+      }
+      ],
+    noRecTrack: false,
+    overrideCopyright: "",
+
+
+    localBiblio: {
+      "ifcOWL":{
+        href:"http://openbimstandards.org/standards/ifcowl/",
+        title:" Web Ontology Language (OWL) representation of the Industry Foundation Classes (IFC) schema",
+        authors: ["Pieter Pauwels", "Walter Terkaj", "Jakob Beetz", "María Poveda Villalón", "Seppo Törmä", "Jyrki Oraskari", "Nam Vu Hoang"],
+        date: "16 June 2008"
+       }
+    },
+    issueBase: ""
+};
