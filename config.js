3d223e3636feb2313d9145c289e711a8ed6b3068
Mads Holten Rasmussen <mads.h.rasmussen@gmail.com>
2018-06-14 09:48:51 +0200

    copied most recent state from lbd/bot

--- config.js
+++ config.js
@@ -20,14 +20,18 @@ var respecConfig = {
         company: "Ghent University",
         url: "https://www.ugent.be/ea/architectuur/en/contact/staff-members/pieter-pauwels",
         companyURL: "https://www.ugent.be/en"
+    },
+    {
+        name: "Maxime Lefrançois",
+        company: "École Nationale Supérieure des Mines de Saint-Étienne",
+        url: "http://www.maxime-lefrancois.info/",
+        companyURL: "https://www.mines-stetienne.fr/"
     }],
 	otherLinks: [
       {
       key: "Contributors (ordered alphabetically)",
       data: [
       {
-        value: "Maxime Lefrançois, École Nationale Supérieure des Mines de Saint-Étienne"
-      },{
         value: "Georg Ferdinand Schneider, Fraunhofer Institute for Building Physics, Technische Hochschule Nürnberg"
       }]
       },
@@ -58,6 +62,13 @@ var respecConfig = {
         title:" Web Ontology Language (OWL) representation of the Industry Foundation Classes (IFC) schema",
         authors: ["Pieter Pauwels", "Walter Terkaj", "Jakob Beetz", "María Poveda Villalón", "Seppo Törmä", "Jyrki Oraskari", "Nam Vu Hoang"],
         date: "16 June 2008"
+       },
+      "LBD-UCR":{
+        href:"https://w3c-lbd-cg.github.io/lbd/UseCasesAndRequirements/",
+        type: "Draft Community Group Report",
+        title:"Linked Building Data Community Group Use Cases & Requirements",
+        authors: ["Linked Building Data Community Group"],
+        date: "ongoing work"
        }
     },
     issueBase: ""
